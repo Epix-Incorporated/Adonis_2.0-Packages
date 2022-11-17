16099cf93f005b5c8b57616bf4cc264ec17ddbb5
Sceleratis <sceleratis@gmail.com>
2022-11-17 17:33:17 -0500

    Update metadata.lua

--- Packages/metadata.lua
+++ Packages/metadata.lua
@@ -1,6 +1,5 @@
 return {
-    "Name": "Epix Incorporated - Official",
-    "Description": "Official Adonis_2.0 Package Repository",
-    "ContainerModelId": 0
-  }
-  
+    Name = "Epix Incorporated - Official",
+    Description = "Official Adonis_2.0 Package Repository",
+    ContainerModelId = 0
+}
